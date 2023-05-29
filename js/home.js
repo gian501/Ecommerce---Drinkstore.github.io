fe6190ac98eea40cb270e1929c6e736ddf9153ff
Gianfranco Valente Cano <gianny@MBPdeGianfranco.lan>
2023-05-29 10:06:28 -0300

    Checkpoint. todo funcionando, se modifico el dark-mode.Vemos el metodo de pago

--- js/home.js
+++ js/home.js
@@ -116,3 +116,26 @@ const filtroProduct = document.querySelectorAll(".boton-categoria");
         }
     })
 });
+
+const bsColor = document.querySelector('#switch');
+
+bsColor.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	bsColor.classList.toggle('active');
+
+	// Guardamos el modo en localstorage.
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	bsColor.classList.add('active');
+} else {
+	document.body.classList.remove('dark');
+	bsColor.classList.remove('active');
+}
